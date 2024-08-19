abf90703d8a9a484fe36e25dfe1eb9c1e3f51d1c
airtonbjunior <airtonbjunior@gmail.com>
2024-08-19 08:55:34 -0300

    Validação email

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ function submeter() {
     let elementoNome = document.getElementById("nome").value;
     let elementoIdade = document.getElementById("idade").value;
     let elementoEmail = document.getElementById("email").value;
-    console.log(elementoNome);
-    console.log(elementoIdade);
-    console.log(elementoEmail);
+    //console.log(elementoNome);
+    //console.log(elementoIdade);
+    //console.log(elementoEmail);
+
+    console.log(validaEmail(elementoEmail));
+}
+
+function validaEmail(email) {
+    return /^[\w]+@[\w]+.[a-zA-Z]+$/.test(email)
 }
